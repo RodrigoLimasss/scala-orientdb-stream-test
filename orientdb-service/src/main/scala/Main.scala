4424d750ceed1e61367754f29f97e32c576933fb
Rodrigo Lima <rlsilva@stone.com.br>
2016-08-25 16:37:39 -0300

    Create a event listener to receive operations from orientdb

--- orientdb-service/src/main/scala/Main.scala
+++ orientdb-service/src/main/scala/Main.scala
@@ -1,8 +1,65 @@
+import com.orientechnologies.orient.core.db.document.ODatabaseDocumentTx
+import com.orientechnologies.orient.core.db.record.ORecordOperation
+import com.orientechnologies.orient.core.record.impl.ODocument
+import com.orientechnologies.orient.core.sql.query.{OResultSet, OLiveQuery, OLiveResultListener}
+
+import scala.concurrent.Future
+import scala.io.StdIn
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
+
 /**
   * Created by Rodrigo Lima on 24/08/2016.
   */
+
 object Main {
-  def main(args: Array[String]){
-    println("Project Initiate...")
+
+  private val uri: String = "remote:10.152.20.212/StoneFraudDetection"
+
+  def main(args: Array[String]) {
+
+    def livePerson(name: String): Unit = Future {
+
+      val db: ODatabaseDocumentTx = new ODatabaseDocumentTx(uri)
+      db.open("admin", "admin")
+
+      val listener: MyLiveQueryListener = new MyLiveQueryListener(db)
+      val result: OResultSet[ODocument] = db.query(new OLiveQuery[ODocument]("live select from Person", listener))
+
+      ResultListener(name, result.get(0).field("token"))
+
+    } onComplete {
+      case Success(s) => println(s"Toke ${s.name}: ${s.token}")
+      case Failure(e) => e.printStackTrace()
+    }
+
+    livePerson("1")
+    livePerson("2")
+
+    StdIn.readLine()
   }
 }
+
+final case class ResultListener(name: String, token: Int)
+
+final class MyLiveQueryListener(db: ODatabaseDocumentTx) extends OLiveResultListener {
+
+  override def onLiveResult(iLiveToken: Int, iOp: ORecordOperation): Unit = {
+    if (!db.isActiveOnCurrentThread) db.activateOnCurrentThread
+
+    var msg: String = "\nNew result from server for live query " + iLiveToken + "\n"
+    msg += "operation: " + iOp.`type` + "\n"
+    msg += "content: " + iOp.record
+
+    println(msg)
+  }
+
+  def onError(iLiveToken: Int): Unit = {
+    println("Live query terminate due to error")
+  }
+
+  def onUnsubscribe(iLiveToken: Int): Unit = {
+    println("Live query terminate with unsubscribe")
+  }
+}
+
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,6 +3,7 @@ import sbt._
 object Dependencies{
 
   val commonDependencies : Seq[ModuleID] = Seq(
+    "com.orientechnologies" % "orientdb-graphdb" % "2.2.8",
     "com.marekkadek" %% "orientdb-scala-stream" % "0.5.4"
   )
 
